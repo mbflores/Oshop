1f3f23fd4ce06b5d57eb84b7c705bffb005735d8
melbryanflores <mel.flores@iridium-technology.com>
2020-08-01 13:02:03 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,51 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthGuardModule} from '@angular/fire/auth-guard';
+import {environment} from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {Router, RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthGuardModule,
+    RouterModule.forRoot([
+        {path: '', component: HomeComponent},
+        {path: 'products', component: ProductsComponent},
+        {path: 'shopping-cart', component: ShoppingCartComponent},
+        {path: 'check-out', component: CheckOutComponent},
+        {path: 'order-success', component: OrderSuccessComponent},
+        {path: 'login', component: LoginComponent},
+        {path: 'admin/products', component: AdminProductsComponent},
+        {path: 'admin/orders', component: AdminOrdersComponent},
+        ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCfl5eIo7tyLMZGIRsEpOSZNJvpfy3HT4Y',
+    authDomain: 'oshopv2-57ba2.firebaseapp.com',
+    databaseURL: 'https://oshopv2-57ba2.firebaseio.com',
+    projectId: 'oshopv2-57ba2',
+    storageBucket: 'oshopv2-57ba2.appspot.com',
+    messagingSenderId: '79082447398',
+    appId: '1:79082447398:web:ce64cef40dcb6809367ce7',
+    measurementId: 'G-37D7Z6JX82'
+  }
 };
